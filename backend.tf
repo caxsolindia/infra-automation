97e1a3309e11ba88ddd189b13d75b35f6c480636
devopscaxsol <108056553+devopscaxsol@users.noreply.github.com>
2025-03-20 14:17:45 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.10.0"
   backend "s3" {
     encrypt      = true
-    bucket       = "edc-terraform-demo"
-    region       = "ap-south-1"
-    key          = "terraform.tfstate"
+    bucket       = ""
+    region       = ""
+    key          = ""
     use_lockfile = true
   }
 }
